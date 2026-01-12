85abe37d4ee2d327ebb419a05b56a91c17e91433
Fatima <fat.tka4enko2014@yandex.ru>
2026-01-12 16:41:37 +0300

    feat: add delivery card order tests

---
+++ src/test/java/ru/netology/delivery/test/SimpleTest.java
@@ -0,0 +1,10 @@
+package ru.netology.delivery.test;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+    @Test
+    void shouldPass() {
+        System.out.println("Test passed!");
+    }
+}
